2112a65061a648ec7e6d194bb926e1f78b7d21ce
André Silveira <andre.silveira1silva@gmail.com>
2023-04-28 15:45:23 -0300

    Update project with components and create home and datails page.

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from './styles'
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/andresilveira1.png"
+          alt="Foto do usuário"
+        />
+        <div>
+          <span>Bem vindo,</span>
+          <strong>André Silveira</strong>
+        </div>
+      </Profile>
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  )
+}
