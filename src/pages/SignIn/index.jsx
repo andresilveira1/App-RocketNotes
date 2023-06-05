fc8da541bbcb3b37c36e4064d310d654f69ea79d
André Silveira <andre.silveira1silva@gmail.com>
2023-06-05 13:59:22 -0300

    feature: SignIn - Interaction to check if user info match with data on db.

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,23 @@
 import { Container, Form, Background } from './styles'
-
 import { FiMail, FiLock } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+import { useAuth } from '../../hooks/auth'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { Link } from 'react-router-dom'
-
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useAuth()
+
+  function handleSignIn() {
+    signIn({ email, password })
+  }
+
   return (
     <Container>
       <Form>
@@ -15,10 +25,20 @@ export function SignIn() {
         <p>Aplicação para salvar e gerenciar seu links úteis.</p>
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
