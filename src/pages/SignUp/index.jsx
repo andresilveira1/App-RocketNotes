d75208b3549df4fcfde9331f7a74e533e6aa5363
André Silveira <andre.silveira1silva@gmail.com>
2023-06-05 13:40:41 -0300

    feature: Communication with backend - Create user and save on database.

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,13 +1,44 @@
 import { Container, Form, Background } from './styles'
-
 import { FiUser, FiMail, FiLock } from 'react-icons/fi'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+
+import { api } from '../../services/api'
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { Link } from 'react-router-dom'
-
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert('Preencha todos os campos!')
+    }
+
+    api
+      .post('/users', { name, email, password })
+      .then((success) => {
+        if (success) {
+          alert('Usuário cadastrado com sucesso!')
+          navigate('/')
+        } else {
+          navigate('/register')
+        }
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert('Não foi possível cadastrar.')
+        }
+      })
+  }
+
   return (
     <Container>
       <Background />
@@ -16,11 +47,26 @@ export function SignUp() {
         <p>Aplicação para salvar e gerenciar seu links úteis.</p>
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={handleSignUp} />
 
         <Link to="/">Voltar para o login</Link>
       </Form>
