2d43c262a125500def352075d1cfe0856a63d5ed
André Silveira <andre.silveira1silva@gmail.com>
2023-06-06 20:30:15 -0300

    feature: App SignOut and user avatar config.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,33 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
+import { useAuth } from '../../hooks/auth'
+import { api } from '../../services/api'
+import { useNavigate } from 'react-router-dom'
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 
 export function Header() {
+  const { signOut, user } = useAuth()
+  const navigate = useNavigate()
+
+  function handleSignOut() {
+    navigate('/')
+    signOut()
+  }
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder
   return (
     <Container>
       <Profile to="/profile">
-        <img
-          src="https://github.com/andresilveira1.png"
-          alt="Foto do usuário"
-        />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Bem vindo,</span>
-          <strong>André Silveira</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
-      <Logout>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
